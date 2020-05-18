e6b7032298ae5d42db5035918278b3dc703f806d
Czadzik <dns997@gmail.com>
2020-05-18 22:15:35 +0200

    Poprawne pokazywanie paska zrowia przeciwników, zmiana nazwy GameState na DetalisOfSpecyficEnemy

--- enemy.js
+++ enemy.js
@@ -1,43 +1,41 @@
 class Enemy {
     flyDirection = 'right';
-    constructor(x, y, shootProbability, speed,texture,width,height,hp) {
+
+    constructor(x, y, shootProbability, speed, texture, width, height, hp) {
         this.x = x;
         this.y = y;
         this.shootProbability = shootProbability;
-        this.texture=texture;
+        this.texture = texture;
         this.width = width;
         this.height = height;
         this.vx = speed;
         this.vy = speed;
-        this.hp=hp;
-        this.hitted=false;
+        this.hp = hp;
+        this.startHp = hp;
+        this.hitted = false;
     };
 
     setupEnemyShots(enemyShots) {
         this.enemyShots = enemyShots;
     }
-    showEnemyLife()
-    {
-        if (this.hitted&&this.hp!==0)
-        {
-            rect(this.x,this.y,this.width,3);
+
+    showEnemyLife() {
+        if (this.hitted && this.hp !== 0) {
+            fill (255, 0, 0);
+            rect (this.x, this.y, this.hp * 100 / this.startHp, 3);
         }
     }
 
-
-    isHitted(){
+    isHitted() {
         this.hp--;
-        this.hitted=true;
-        if (this.hp===0)
-        {
+        this.hitted = true;
+        if (this.hp === 0) {
             return true;
-        }
-      else{
+        } else {
             return false;
         }
 
-
-}
+    }
 
     move() {
         this.y += this.vy;
@@ -49,24 +47,24 @@ class Enemy {
         if (this.x === toX && this.y === toY)
             return true;
 
-        if (Math.abs(this.x - toX) < Math.abs(this.vx))
+        if (Math.abs (this.x - toX) < Math.abs (this.vx))
             this.x = toX;
         else {
-            if((this.x - toX) > 0)
-                this.vx = (this.vx > 0)? -this.vx: this.vx;
-            else{
-                this.vx = (this.vx > 0)? this.vx: -this.vx;
+            if ((this.x - toX) > 0)
+                this.vx = (this.vx > 0) ? -this.vx : this.vx;
+            else {
+                this.vx = (this.vx > 0) ? this.vx : -this.vx;
             }
             this.x += this.vx;
         }
 
-        if (Math.abs(this.y - toY) < Math.abs(this.vy))
+        if (Math.abs (this.y - toY) < Math.abs (this.vy))
             this.y = toY;
         else {
-            if((this.y - toY) > 0)
-                this.vy = (this.vy > 0)? -this.vy: this.vy;
-            else{
-                this.vy = (this.vy > 0)? this.vy: -this.vy;
+            if ((this.y - toY) > 0)
+                this.vy = (this.vy > 0) ? -this.vy : this.vy;
+            else {
+                this.vy = (this.vy > 0) ? this.vy : -this.vy;
             }
             this.y += this.vy;
         }
@@ -74,29 +72,29 @@ class Enemy {
     }
 
     show() {
-        image(this.texture, this.x, this.y, this.width, this.height)
+        image (this.texture, this.x, this.y, this.width, this.height)
     };
 
     attemptShooting() {
-        let rand = random(); // random() bez argumentow zwraca float z przedzialu <0;1)
+        let rand = random (); // random() bez argumentow zwraca float z przedzialu <0;1)
         if (rand < this.shootProbability) {
-            this.shoot();
+            this.shoot ();
 
         }
     };
 
     shoot() {
-        this.enemyShots.push(new EnemyShot(this.x + 35, this.y, this.vx))
+        this.enemyShots.push (new EnemyShot (this.x + 35, this.y, this.vx))
     };
 
-    flyToRightAndBack(leftX, rightX){
-        if(this.flyDirection === 'right'){
-            if(this.flyTo(rightX, this.y))
+    flyToRightAndBack(leftX, rightX) {
+        if (this.flyDirection === 'right') {
+            if (this.flyTo (rightX, this.y))
                 this.flyDirection = 'left';
         }
 
-        if(this.flyDirection === 'left'){
-            if(this.flyTo(leftX, this.y)){
+        if (this.flyDirection === 'left') {
+            if (this.flyTo (leftX, this.y)) {
                 this.flyDirection = 'right';
             }
         }
